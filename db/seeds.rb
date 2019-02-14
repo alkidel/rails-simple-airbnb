9b0ddfc923feb2e6d2a914bf34892e0b929c54ea
Alki Delichatsios <alkidel@gmail.com>
2019-02-14 09:12:30 +0100

    rails simple airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+flats_attributes = [
+  {
+    name: 'Light & Spacious Garden Flat London',
+    address: '10 Clifton Gardens London W9 1DT',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 75,
+    number_of_guests: 3
+  },
+  {
+    name: 'Funky house in Boston',
+    address: 'Boston, Mass',
+    description: 'Come have fun in this funky house in Boston',
+    price_per_night: 100,
+    number_of_guests: 6
+  },
+  {
+    name: 'Parisian Castle',
+    address: 'Bastille, Paris',
+    description: 'Come live in this castle for a weekend',
+    price_per_night: 50,
+    number_of_guests: 4
+  },
+  {
+    name: 'Greek beachouse',
+    address: 'Athens, Greece',
+    description: 'We can drink ouzo and dance by the beach!',
+    price_per_night: 20,
+    number_of_guests: 1
+  }
+
+]
+Flat.create!(flats_attributes)
+puts 'Finished!'
